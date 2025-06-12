a3c4c89b2aea491543c68420b317e9d0e9a98e3f
fern <179273869+Fern-labs@users.noreply.github.com>
2025-06-11 22:59:33 -0700

    Added skeleton css and html

--- libary.js
+++ libary.js
@@ -1,1 +1,15 @@
-console.log("work");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
