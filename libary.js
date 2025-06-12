4495bd0d7d84b3e9a7c5522b37be0b09930b7f3f
fern <179273869+Fern-labs@users.noreply.github.com>
2025-06-12 01:48:58 -0700

    JavaScript working book info gets added to card correctly

--- libary.js
+++ libary.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
+  this.id = self.crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -10,6 +11,32 @@ function Book(title, author, pages, read) {
   };
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
+
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }
+
+addBookToLibrary("Red Rising", "Pierce Brown", 416, "read");
+addBookToLibrary("Golden Son", "Pierce Brown", 464, "not read");
+addBookToLibrary("Morning Star", "Pierce Brown", 576, "not read");
+console.log(myLibrary);
+
+//creates a new book div for each book object
+//displays title author pages and read
+//does this for the entire array when this is ran
+
+function updateBookShelf() {
+  for (let book of myLibrary) {
+    const bookShelf = document.querySelector(".bookshelf");
+    let addBook = document.createElement("div");
+    addBook.classList.add("book");
+    //for later book.title works for css
+    //addBook.innerHTML = book.title;
+    addBook.innerHTML = book.info();
+    bookShelf.appendChild(addBook);
+  }
+}
+
+updateBookShelf();
